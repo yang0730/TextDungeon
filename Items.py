69dfce47d6510eaa057dfcd3ca93db6981d844e2
yanglin <31237340+yang0730@users.noreply.github.com>
2022-07-07 03:46:04 +0000

    Item read and search

--- Items.py
+++ Items.py
@@ -6,11 +6,14 @@ for line in lines:
   line = line.strip("\n")
   line = line.split(",")
   line[0] = int(line[0])
+  line[1] = line[1].strip("\'")
   line[2] = int(line[2])
   line[3] = int(line[3])
   line[4] = int(line[4])
   line[5] = int(line[5])
   line[6] = int(line[6])
+  line[7] = line[7].strip("\'")
+  line[8] = line[8].strip("\'")
   line[9] = int(line[9])
   Items.append(line)
 
@@ -20,4 +23,3 @@ def findItem(i):
     if item[0] == i:
       return item
 
-print(findItem(2500))
